a674324151481f11f3c7530d8ea1680ab2a0cbb1
石翔 <1556004626@qq.com>
2023-04-11 20:36:49 +0800

    施工记录 bean

--- app/src/main/java/com/sgd/tjlb/zhxf/entity/ConstructionRecordBean.java
+++ app/src/main/java/com/sgd/tjlb/zhxf/entity/ConstructionRecordBean.java
@@ -6,8 +6,14 @@ import android.text.TextUtils;
  * 施工记录
  */
 public class ConstructionRecordBean {
+
+   private String device_work_id;//记录ID
+   private String device_id;//设备ID
    private String status_img;//施工图片
-   private String status_info;//施工内容
+   private String status_info;//描述
+   private int status;//0成功，2安装异常，4维修异常
+   private String shop_name;//地址名称
+   private String address;//门店地址
    private String create_time;//时间
 
    public String getStatus_img() {
@@ -32,5 +38,45 @@ public class ConstructionRecordBean {
 
    public void setCreate_time(String create_time) {
       this.create_time = create_time;
+   }
+
+   public String getDevice_work_id() {
+      return device_work_id;
+   }
+
+   public void setDevice_work_id(String device_work_id) {
+      this.device_work_id = device_work_id;
+   }
+
+   public String getDevice_id() {
+      return device_id;
+   }
+
+   public void setDevice_id(String device_id) {
+      this.device_id = device_id;
+   }
+
+   public int getStatus() {
+      return status;
+   }
+
+   public void setStatus(int status) {
+      this.status = status;
+   }
+
+   public String getShop_name() {
+      return shop_name;
+   }
+
+   public void setShop_name(String shop_name) {
+      this.shop_name = shop_name;
+   }
+
+   public String getAddress() {
+      return address;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
    }
 }
